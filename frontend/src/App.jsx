99e5c6f24d33f9b39099cfa3d1044f741a3cbce7
Henry Valle <alexandervh1986@gmail.com>
2025-06-13 06:43:07 +0200

    pages config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import './css/App.css'
+import '../src/css/App.css'
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import { MovieProvider } from './contexts/MovieContext';
 import NavBar from './components/NavBar';
 
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar/>
   <main className='main-content'>
       <Routes>
@@ -15,7 +16,7 @@ function App() {
         <Route path='/favorites' element={<Favorites/>}/>
       </Routes>
   </main>
-  </div>
+  </MovieProvider>
   )
 }
 
--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,23 @@
 import '../css/Favorites.css'
+import { useMovieContext } from '../contexts/MovieContext'
+import MovieCard from '../components/MovieCard'
+
+
 
 function favorite() {
+  const {favorites} = useMovieContext()
+
+  if (favorites) {
+    return (
+      <div className='favorites'>
+    <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+        </div>)
+  }
+
   return (
     <div className="favorites-empty">
       <h2>No favorite movies yet</h2>
